479b7dca7306681b0037b552e53b2cd6daab286e
Steven Hobson-Campbell <xapphire13@live.com>
2020-02-20 22:46:05 -0800

    improve name detection

--- src/utils/getComponentName.ts
+++ src/utils/getComponentName.ts
@@ -2,7 +2,8 @@ import React from 'react';
 
 export default function(component: React.ReactNode): string {
   return (
-    (component as any).type.displayName ||
+    (component as any).type?.displayName ||
+    (component as any).type?.name ||
     (component as any).name ||
     'Component'
   );
